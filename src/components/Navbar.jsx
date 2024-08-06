978685871aecafe979acdf322cca2799ea86b7c6
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-06 21:17:53 +0530

    navbar setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className=" mb-20 flex items-center justify-between py-6">
+        <div className="flex flex-shrink-0 items-center">
+            <h2>My Portfolio</h2>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
