d1c61c905f26c3c9e6b7ec5d0d2f402207dba5d6
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-07 17:34:37 +0530

    Navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,26 @@
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <h2>My Portfolio</h2>
-        </div>
+      <div className="flex flex-shrink-0 items-center">
+        <h2
+          style={{ color: "white", fontWeight: "bold" , fontSize: '26px'  }}
+          className="color: white"
+        >
+          My Portfolio
+        </h2>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <FaLinkedin />
+        <FaGithub />
+        <FaSquareXTwitter />
+        <FaInstagram />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
