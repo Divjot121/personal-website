e4889b4bafb1821301c9dc6938e311be6db9e7e2
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-10 13:18:54 +0530

    fixed overlapping issues

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { PROJECTS } from "../constants";
 import { motion } from "framer-motion";
+
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
@@ -14,8 +15,11 @@ const Projects = () => {
       </motion.h1>
       <div>
         {PROJECTS.map((project, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+          <div
+            key={index}
+            className="mb-12 flex flex-wrap lg:justify-center lg:space-x-8"
+          >
+            <div className="w-full lg:w-1/4 mb-4 lg:mb-0">
               <motion.img
                 whileInView={{ opacity: 1, x: 0 }}
                 initial={{ opacity: 0, x: -100 }}
@@ -25,7 +29,7 @@ const Projects = () => {
                 width={300}
                 height={300}
                 className="rounded-md"
-              ></motion.img>
+              />
             </div>
             <motion.div
               whileInView={{ opacity: 1, x: 0 }}
@@ -33,26 +37,28 @@ const Projects = () => {
               transition={{ duration: 1 }}
               className="w-full max-w-xl lg:w-3/4"
             >
-              <h6 className="mb-2 mt-2 font-semibold">{project.title}</h6>
-              <p className="mb-4 text-neutral-400 ">{project.description}</p>
-              {project.technologies.map((tech, index) => (
-                <span
-                  key={index}
-                  className="mr-2 rounded-md bg-neutral-800 text-purple-500 px-2 py-1 text-sm font-medium"
-                >
-                  {tech}
-                </span>
-              ))}
+              <h6 className="mb-4 font-semibold text-xl">{project.title}</h6>
+              <p className="mb-4 text-neutral-400">{project.description}</p>
+              <div className="mb-4 flex flex-wrap space-x-2">
+                {project.technologies.map((tech, index) => (
+                  <span
+                    key={index}
+                    className="mb-2 rounded-md bg-neutral-800 text-purple-500 px-2 py-1 text-sm font-medium"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
               <div className="mb-2 mt-2 pt-2">
                 <a
                   href={project.github}
-                  className="mr-2 rounded-md bg-green-900 text-white px-2 py-1"
+                  className="mr-2 rounded-md bg-green-900 text-white px-4 py-2"
                 >
                   Github
                 </a>
                 <a
                   href={project.live}
-                  className="mr-2 rounded bg-cyan-950 text-white px-2 py-1"
+                  className="rounded bg-cyan-950 text-white px-4 py-2"
                 >
                   Live
                 </a>
