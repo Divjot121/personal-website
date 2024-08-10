30a408f79106eaee724984f643778d36a3b1936a
Divjot Singh Arora <88025671+Divjot121@users.noreply.github.com>
2024-08-10 10:49:01 +0530

    favicon and title updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import { motion } from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 const Footer = () => {
-    return (
-        <footer className='pt-14 pb-14'>
-          <motion.div whileInView={{opacity:1 ,x:0}}
-          initial={{opacity:0,x:-100}}
-          transition={{duration:1}} className="container mx-auto text-center">
-            <p className="text-white text-xl">
-               Made with <span className="text-red-500">❤</span> by Divjot Singh Arora
-            </p>
-          </motion.div>
-        </footer>
-      );
-}
+  return (
+    <footer className="pt-14 pb-14">
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1 }}
+        className="container mx-auto text-center"
+      >
+        <p className="text-white text-xl">
+          Made with <span className="text-red-500">❤</span> by Divjot Singh
+          Arora
+        </p>
+      </motion.div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
